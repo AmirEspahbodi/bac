79b92b35ad0ca401562dba1a09f4408d28b7615a
a.espahbodi <dev.amir.espahbodi@gmail.com>
2025-06-27 09:57:01 +0330

    change validation function epoch

--- src/utils/validation_epoch.py
+++ src/utils/validation_epoch.py
@@ -6,7 +6,6 @@ from .metrics import calculate_mrr_fn, top_k_accuracy_fn
 def validation_epoch_fn(model, data_loader, loss_fn, device, description="", k_for_top_k_eval=None):
     model.eval()
     epoch_loss = AverageMeter()
-    epoch_accuracy_top1 = AverageMeter()
 
     all_predictions_eval = []
     all_labels_eval = []
@@ -27,18 +26,21 @@ def validation_epoch_fn(model, data_loader, loss_fn, device, description="", k_f
 
             epoch_loss.update(loss.item())
 
-            # _, predicted = torch.max(outputs.data, 1)
+            _, predicted = torch.max(outputs.data, 1)
             total += targets.size(0)
-            correct += (outputs == targets).sum().item()
+            correct += (predicted == targets).sum().item()
 
-            acc_top1 = (outputs.argmax(dim=1) == targets).float().mean().item()
-            epoch_accuracy_top1.update(acc_top1, targets.size(0))
+            total += targets.size(0)
+            correct += (predicted == targets).sum().item()
 
-            all_predictions_eval.append(outputs)
+            all_predictions_eval.append(predicted)
             all_labels_eval.append(targets)
-            # Corrected line:
-            progress_bar.set_postfix(loss=f"{epoch_loss.avg:.4f}", acc=f"{epoch_accuracy_top1.avg:.4f}")
+            
+            # Corrected line
+            progress_bar.set_postfix(loss=f"{epoch_loss.avg:.4f}", acc=f"{100 * correct / total:.4f}")
 
+    accuracy = 100 * correct / total
+    
     progress_bar.close()
     calculated_top_k_acc = None
     calculated_mrr = 0.0
@@ -48,4 +50,4 @@ def validation_epoch_fn(model, data_loader, loss_fn, device, description="", k_f
         calculated_mrr = calculate_mrr_fn(all_predictions_tensor, all_labels_tensor)
         if k_for_top_k_eval is not None:
             calculated_top_k_acc = top_k_accuracy_fn(all_predictions_tensor, all_labels_tensor, k=k_for_top_k_eval)
-    return epoch_loss.avg, epoch_accuracy_top1.avg, calculated_mrr, calculated_top_k_acc
+    return epoch_loss.avg, accuracy, calculated_mrr, calculated_top_k_acc
