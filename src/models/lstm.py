24a530397ba52d9c6f22f8ad71993841c89cb429
a.espahbodi <dev.amir.espahbodi@gmail.com>
2025-06-29 11:11:02 +0330

    change lstm model

--- src/models/lstm.py
+++ src/models/lstm.py
@@ -11,15 +11,16 @@ class LSTMModel(nn.Module):
                              hidden_size=hidden_size,
                              num_layers=num_layers,
                              bidirectional=bidirectional,
-                             batch_first=True,
+                             batch_first=False,
                              dropout=dropout_rate if num_layers > 1 else 0) 
     fc_input_features = hidden_size * 2 if bidirectional else hidden_size
     self.fc = nn.Linear(fc_input_features, num_cls) 
     self.dropout_fc = nn.Dropout(dropout_rate)
 
   def forward(self, x):
-    outputs, _ = self.rnn(x)
-    out = outputs.mean(dim=1)
+    outputs, _ = self.rnn(x) # outputs: (seq_len, batch, num_directions * hidden_size)
+    # Aggregate RNN outputs. Using mean pooling over time steps.
+    out = outputs.mean(dim=0) # Shape: (batch, num_directions * hidden_size)
     out = self.dropout_fc(out)
-    y = self.fc(out) 
+    y = self.fc(out) # Shape: (batch, num_cls)
     return y
