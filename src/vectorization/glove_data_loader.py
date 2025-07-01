30974d436449c54ad140ead83c7899b0eca00b0b
a.espahbodi <dev.amir.espahbodi@gmail.com>
2025-07-01 11:30:33 +0330

    prints for bug fixing in kaggle

--- src/vectorization/glove_data_loader.py
+++ src/vectorization/glove_data_loader.py
@@ -98,7 +98,7 @@ def ensure_glove_file_is_present():
                 )
                 logging.info(f"Available files: {zip_ref.namelist()}")
                 return False
-    except zipfile.BadZipFile:
+    except zipfile.BadZipFile as e:
         print(f"ensure_glove_file_is_present 6, 6, 6, 6, 6, 6. {e}")
         logging.error(
             f"Error: {GLOVE_LOCAL_ZIP_PATH} is a bad zip file. Please delete it and try again."
